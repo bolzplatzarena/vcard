f558c442bfb602c0f04fb1661b52247251046d59
Thomas Renger <thomas.renger@gmail.com>
2022-11-06 00:00:39 +0100

    feat: basic layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+const plugin = require("tailwindcss/plugin");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -6,5 +7,24 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addUtilities }) {
+      const safeArea = {
+        ".p-safe-top": {
+          paddingTop: "env(safe-area-inset-top)"
+        },
+        ".p-safe-left": {
+          paddingLeft: "env(safe-area-inset-left)"
+        },
+        ".p-safe-right": {
+          paddingRight: "env(safe-area-inset-right)"
+        },
+        ".p-safe-bottom": {
+          paddingBottom: "env(safe-area-inset-bottom)"
+        },
+      };
+
+      addUtilities(safeArea);
+    })
+  ],
 }
