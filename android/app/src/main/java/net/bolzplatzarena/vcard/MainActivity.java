e2146742be10f4fb513716ce08102f4bff4a556a
Thomas Renger <thomas.renger@gmail.com>
2022-11-05 16:32:42 +0100

    feat: base setup for the apps

---
+++ android/app/src/main/java/net/bolzplatzarena/vcard/MainActivity.java
@@ -0,0 +1,5 @@
+package net.bolzplatzarena.vcard;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'net.bolzplatzarena.vcard',
+  appName: 'vcard',
+  webDir: 'dist/vcard',
+  bundledWebRuntime: false
+};
+
+export default config;
