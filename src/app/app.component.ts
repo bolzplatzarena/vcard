eb4bad3010c02b43b0f1fd53ad2d92bdd54ffdf5
Thomas Renger <thomas.renger@gmail.com>
2022-12-08 21:11:52 +0100

    feat: with download

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { NgxQrcodeStylingService, Options } from 'ngx-qrcode-styling';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -34,6 +35,24 @@ export class AppComponent {
       margin: 0
     }
   };
+
+  readonly qrDownloadConfig: Options = {
+    width: screen.width,
+    height: screen.height,
+    margin: 50,
+    dotsOptions: {
+      color: "#1977f3",
+      type: "dots"
+    },
+    backgroundOptions: {
+      color: "#ffffff",
+    },
+    imageOptions: {
+      crossOrigin: "anonymous",
+      margin: 0
+    }
+  };
+
   qrData?: string;
 
   constructor(
@@ -41,12 +60,11 @@ export class AppComponent {
     private readonly qrcode: NgxQrcodeStylingService,
   ) {}
 
-  generate(): void {
+  generate(preview = true): Promise<void> {
     if(this.form.invalid) {
-      return;
+      return Promise.reject();
     }
 
-    this.qrData = undefined;
     const person = this.form.getRawValue();
 
     const vcard = `BEGIN:VCARD
@@ -55,17 +73,17 @@ N:${person.familyName};${person.givenName}
 FN:${person.givenName} ${person.familyName}
 END:VCARD`;
 
-    this.qrcode.create({
-        ...this.qrConfig,
+    return firstValueFrom(this.qrcode.create({
+        ...(preview ? this.qrConfig : this.qrDownloadConfig),
         data: vcard,
         width: this.canvas.nativeElement.clientWidth
       },
       this.canvas.nativeElement
-    ).subscribe();
-    this.qrData = vcard;
+    ));
    }
 
-   close(): void {
-     this.qrData = undefined;
+   async download(): Promise<void> {
+     await this.generate(false);
+     this.qrcode.download("vcard.png", this.canvas.nativeElement).subscribe();
    }
 }
