475e2b0c2deb246807974f37c9312f6961af6ac5
Thomas Renger <thomas.renger@gmail.com>
2022-12-08 20:42:00 +0100

    feat: add qr code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,71 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { NgxQrcodeStylingService, Options } from 'ngx-qrcode-styling';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styles: ['.ng-invalid.ng-touched { border-color: red }']
 })
 export class AppComponent {
+  @ViewChild("canvas", { static: false }) canvas!: ElementRef;
+  readonly form = this.formBuilder.nonNullable.group({
+    givenName: ['', [Validators.required]],
+    familyName: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    phone: [''],
+    mobile: ['', [Validators.required]],
+    address: [''],
+  });
+  readonly qrConfig: Options = {
+    width: 300,
+    height: 300,
+    margin: 50,
+    dotsOptions: {
+      color: "#1977f3",
+      type: "dots"
+    },
+    backgroundOptions: {
+      color: "#ffffff",
+    },
+    imageOptions: {
+      crossOrigin: "anonymous",
+      margin: 0
+    }
+  };
+  qrData?: string;
+
+  constructor(
+    private readonly formBuilder: FormBuilder,
+    private readonly qrcode: NgxQrcodeStylingService,
+  ) {}
+
+  generate(): void {
+    if(this.form.invalid) {
+      return;
+    }
+
+    this.qrData = undefined;
+    const person = this.form.getRawValue();
+
+    const vcard = `BEGIN:VCARD
+VERSION:3.0
+N:${person.familyName};${person.givenName}
+FN:${person.givenName} ${person.familyName}
+END:VCARD`;
+
+    this.qrcode.create({
+        ...this.qrConfig,
+        data: vcard,
+        width: this.canvas.nativeElement.clientWidth
+      },
+      this.canvas.nativeElement
+    ).subscribe();
+    this.qrData = vcard;
+   }
+
+   close(): void {
+     this.qrData = undefined;
+   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxQrcodeStylingModule } from 'ngx-qrcode-styling';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    NgxQrcodeStylingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
